d136b2deda0ff27ac5f2c4a857945278f305a742
Matt <mattlaws1982@gmail.com>
2020-09-24 18:43:55 +0100

    second commit

---
+++ js/paintguru.js
@@ -0,0 +1,28 @@
+
+$(document).ready(function(){
+
+	$('#mobile-nav').click(function(){
+	
+		$('nav').slideToggle(1000);
+	});
+});
+
+
+
+// $('.trigger').click(function() {
+// 	console.log('click working?');
+// 	$('#nav').slideToggle(1000);
+
+// });
+	//Set of instructions
+	
+
+	// hide/show the navigation
+	// .hide(); hide oif on show
+	// .show(); show if currently on hidden
+	// .toggle(); switch between hidden or visable
+	// .slideToggle(); switch with slide animation
+	// .fadeToggle(); switch with a fade animation
+	//optional duration
+
+	
